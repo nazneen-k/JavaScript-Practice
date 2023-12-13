2f90a68f8aab722f225b46f1bc82521da107feda
Nazneen K <117660793+nazneen-k@users.noreply.github.com>
2023-12-13 23:48:28 +0530

    Add files via upload

---
+++ 5 Variable Declaration/script.js
@@ -0,0 +1,7 @@
+//Write a program that declares a variable but doesn't initialize it.
+// Try to access the variable and observe the result.
+
+let variable;
+
+console.log(variable);
+//Output ---> is Undefined
